c18debcc656097b7180db5cd47c858eb5f31b87f
Ariunjargal <abatjarg@icloud.com>
2016-03-20 14:47:31 -0400

    Adding in dot animation

--- ABLoaders.playground/Contents.swift
+++ ABLoaders.playground/Contents.swift
@@ -48,9 +48,46 @@ circle.frame = CGRectMake(
     (container.bounds.size.height - 40) / 2,
     40, 40)
 
-circle.addAnimation(animationGroup, forKey: "animation")
 
-container.layer.addSublayer(circle)
+// =============================================
+// ========= Three dot animation ===============
+// =============================================
 
+for(var i = 0; i < 3; i++)
+{
+    let dot = CALayer()
+    dot.frame = CGRectMake(CGFloat(20 * i + 100), 100, 10, 10)
+    dot.backgroundColor = UIColor.whiteColor().CGColor
+    dot.anchorPoint = CGPointMake(0.5, 0.5);
+    dot.cornerRadius = 5
+    dot.opacity = 1
+    
+    let dotScaleAnimation = CAKeyframeAnimation(keyPath: "transform")
+    dotScaleAnimation.values = [
+        [NSValue(CATransform3D: CATransform3DMakeScale(0.5, 0.5, 0.0))],
+        [NSValue(CATransform3D: CATransform3DMakeScale(1.0, 1.0, 0.0))]
+    ]
+    
+    let dotAnimation = CAKeyframeAnimation(keyPath: "opacity")
+    dotAnimation.keyTimes = [0.0, 0.7]
+    dotAnimation.values = [0.25, 1.0]
+    
+    let dotAnimationGroup = CAAnimationGroup()
+    dotAnimationGroup.removedOnCompletion = false;
+    dotAnimationGroup.autoreverses = true;
+    dotAnimationGroup.beginTime = 0.1 * Double(i)
+    dotAnimationGroup.repeatCount = HUGE
+    dotAnimationGroup.duration = 1
+    dotAnimationGroup.animations = [dotScaleAnimation, dotAnimation]
+    dotAnimationGroup.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+    
+    container.layer.addSublayer(dot)
+    dot.addAnimation(dotAnimationGroup, forKey: "animation")
+}
 
+//circle.addAnimation(, forKey: "animation")
+//
+// container.layer.addSublayer(dot)
+//
+//
 XCPlaygroundPage.currentPage.liveView = container
